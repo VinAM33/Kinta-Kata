1a4b361a000b04afcb17bc37bdc79707ceb0fc3c
morisseau <vincent.morisseau@afnor-ext.org>
2024-06-26 11:31:22 +0200

    Création Infra core unit test

---
+++ KintaKata.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace KintaKata.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
